6ee6e8cc7fba3b0f50566e008123fc03b8692d00
Kevin Mamaqi Kapllani <kevin.mamaqi@90poe.io>
2022-11-18 16:46:55 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Flex } from '@mantine/core'
 import { Body } from './components/layout'
-import { Deg90Block, Deg90x2_120_interiorBlock } from './components/organisms'
+import {
+  Deg90Block,
+  Deg90x2_120_interiorBlock,
+  Deg90x3Block,
+  Deg120x3Block,
+} from './components/organisms'
 
 const App: React.FC = () => {
   return (
     <Body>
       <Flex direction="column">
-        {/* <Deg90Block />
-        <Deg90x3Block /> */}
+        <Deg90Block />
+        <Deg90x3Block />
+        <Deg120x3Block />
         <Deg90x2_120_interiorBlock />
       </Flex>
     </Body>
